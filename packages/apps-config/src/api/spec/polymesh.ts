e35a9e879005a43dd2bcc9f223d12a3d232b1916
Adam Dossa <adam.dossa@gmail.com>
2022-06-15 21:52:08 +0100

    Update schema

--- packages/apps-config/src/api/spec/polymesh.ts
+++ packages/apps-config/src/api/spec/polymesh.ts
@@ -1,9 +1,9 @@
-// Copyright 2017-2022 @polkadot/apps-config authors & contributors
+// Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
-import schema from '@polymathnetwork/polymesh-types';
+import schema from './polymesh_schema.json';
 
 const definitions: OverrideBundleDefinition = {
   rpc: schema.rpc,
