ea6ca007098f9fdf3453debfbd002174e071e352
F-OBrien <francis@polymesh.network>
2024-03-12 18:54:07 +0000

    lint - update copyright year

--- packages/page-addresses/test/pages/addressesPage.tsx
+++ packages/page-addresses/test/pages/addressesPage.tsx
@@ -1,4 +1,4 @@
-// Copyright 2017-2023 @polkadot/page-addresses authors & contributors
+// Copyright 2017-2024 @polkadot/page-addresses authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import type { AccountOverrides as ContactOverrides } from '@polkadot/test-support/types';
