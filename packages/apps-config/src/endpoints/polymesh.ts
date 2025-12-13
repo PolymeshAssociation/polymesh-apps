3d4b6d84c415cac2099c67ee07bab50e1c35f698
F-OBrien <francis@polymesh.network>
2025-12-13 15:27:35 +0000

    feat: only show Polymesh endpoints + update page title

---
+++ packages/apps-config/src/endpoints/polymesh.ts
@@ -0,0 +1,41 @@
+// Copyright 2017-2026 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { EndpointOption } from './types.js';
+
+import { nodesPolymeshSVG } from '../ui/logos/nodes/index.js';
+
+export const polymeshChains: EndpointOption[] = [
+  {
+    info: 'polymesh',
+    providers: {
+      Polymesh: 'wss://mainnet-rpc.polymesh.network'
+    },
+    text: 'Polymesh Mainnet',
+    ui: {
+      color: 'linear-gradient(197deg, #FF2E72, #4A125E)',
+      logo: nodesPolymeshSVG
+    }
+  },
+  {
+    info: 'polymesh',
+    providers: {
+      Polymesh: 'wss://testnet-rpc.polymesh.live'
+    },
+    text: 'Polymesh Testnet',
+    ui: {
+      color: '#43195B',
+      logo: nodesPolymeshSVG
+    }
+  },
+  {
+    info: 'polymesh',
+    providers: {
+      Polymesh: 'wss://staging-rpc.polymesh.dev/'
+    },
+    text: 'Polymesh Staging',
+    ui: {
+      logo: nodesPolymeshSVG
+    }
+  },
+];
